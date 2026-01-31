4e1a67790c623dba5ec5c7d42e88112d5c2cf556
Haku Yuzuki <chadmasseywelshlin@gmail.com>
2026-01-31 13:56:58 +0000

    Add ASGI configuration for logdev project

---
+++ logdev /asgi.py
@@ -0,0 +1,14 @@
+"""
+ASGI config for logdev project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://log-int.wikidot.com/jp-hub
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+application = get_asgi_application()
